18ab3b21860e2fdae3f4b071d430d91a0754a032
Thomas Poncet <thomas.poncet@ensimag.grenoble-inp.fr>
2015-05-27 15:01:12 +0200

    specifying server parameters

--- server.js
+++ server.js
@@ -2,15 +2,34 @@
  * Created by tanguy on 27/05/15.
  */
 
-var express = require("express")
-    , app = express()
-    , http = require("http").createServer(app)
-    , bodyParser = require("body-parser")
-    , io = require("socket.io").listen(http);
+var express = require("express");
+var app = express();
+var http = require("http").createServer(app);
+var bodyParser = require("body-parser");
+var io = require("socket.io").listen(http);;
+
+
+
+//Server's IP address
+app.set("ipaddr", "127.0.0.1");
+
+//Server's port number
+app.set("port", 8080);
+
+//Specify the views folder
+app.set("views", __dirname + "/views");
+
+//View engine is Jade
+app.set("view engine", "jade");
+
+//Specify where the static content is
+app.use(express.static("public", __dirname + "/public"));
 
 app.get('/', function(req, res) {
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World');
 });
 
-app.listen(8080);
+app.listen(app.get("port"), app.get("ipaddr"), function() {
+    console.log("Server up and running. Go to http://" + app.get("ipaddr") + ":" + app.get("port"));
+});
