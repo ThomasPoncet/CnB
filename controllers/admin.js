5ce94cfb16c36f26503047b5057bc5d31f5b3728
ensimag <ensimag@ensilinux.(none)>
2015-05-28 15:26:30 +0200

    modif admin.js

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,7 @@
 /**
  * Created by Tanguy on 27/05/15.
  */
-var DAO = require('../models/DAOZoneWidget.js');
+//var DAO = require('../models/DAOZoneWidget.js');
 
 exports.run=function(req, res, connexion){
     res.render("admin", {zoneWidgetSon : 'ZoneSon', zoneWidgetEcran : 'Ecran'} )
