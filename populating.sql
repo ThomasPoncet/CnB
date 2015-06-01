bd3fbba88c5da0f66debf5175b769fb74538f69f
Thomas Poncet <thomas.poncet@ensimag.grenoble-inp.fr>
2015-06-01 12:00:37 +0200

    advancements on admin music part

--- populating.sql
+++ populating.sql
@@ -1,5 +1,8 @@
 INSERT INTO content (idContent, nomContent, link, idWidget)
-VALUES (1, "bla", "lien", 2);
+VALUES (1, "musique1", "lien", 2);
+
+INSERT INTO content (idContent, nomContent, link, idWidget)
+VALUES (2, "musique2", "lien", 2);
 
 
 INSERT INTO vote_content (idVisitor, idContent)
