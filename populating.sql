63c6d7e6c54397a90bcc294ba9a4e88aeed23bda
Tanguy <tanguy.esteoule@gmail.com>
2015-06-01 18:02:21 +0200

    db OK

--- populating.sql
+++ populating.sql
@@ -1,6 +1,11 @@
 INSERT INTO content (idContent, nomContent, link, idWidget)
-VALUES (1, "bla", "lien", 2);
+VALUES (1, "Daft Punk - Aerodynamic", "lien1", 1);
 
+INSERT INTO content (idContent, nomContent, link, idWidget)
+VALUES (2, "Django Reinhardt - Nuages", "lien2", 1);
+
+INSERT INTO content (idContent, nomContent, link, idWidget)
+VALUES (3, "Pitt Poule - Tears", "lien3", 1);
 
 
 INSERT INTO widgetzone (idWidgetZone, nomWidgetZone)
@@ -29,13 +34,13 @@ INSERT INTO widget (idWidget, nomWidget, idWidgetZone)
 VALUES (6, "meteo", 2);
 
 INSERT INTO vote_content (idVisitor, idContent)
-VALUES ('1', 1);
+VALUES ("1", 1);
 INSERT INTO vote_content (idVisitor, idContent)
-VALUES ('2', 1);
+VALUES ("2", 1);
 INSERT INTO vote_content (idVisitor, idContent)
-VALUES ('3', 1);
+VALUES ("3", 1);
 INSERT INTO vote_content (idVisitor, idContent)
-VALUES ('4', 2);
+VALUES ("4", 2);
 
 -- INSERT INTO widgetzone (idWidgetZone, nomWidgetZone)
 -- VALUES (1, Sound);
