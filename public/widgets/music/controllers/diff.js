044e59ed418a9a63cae172b4c143b5f480d0837f
Thomas Poncet <thomas.poncet@ensimag.grenoble-inp.fr>
2015-06-02 00:41:50 +0200

    autoplay next music in stream ok

--- public/widgets/music/controllers/diff.js
+++ public/widgets/music/controllers/diff.js
@@ -1,3 +1,11 @@
 /**
  * Created by thomas on 01/06/15.
  */
+
+// To play the next song given by diff/stream
+var player = document.getElementById("player");
+player.addEventListener("ended", function(){
+    player.src = "diff/stream";
+    player.currentTime = 0;
+    player.play();
+}, false);
