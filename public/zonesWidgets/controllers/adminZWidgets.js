f2fecbc8c419698d15c7e4e8b4a61bf38478ebc6
Lucas Bchini <lucas.bchini@ensimag.grenoble-inp.fr>
2015-06-05 10:14:33 +0200

    update moved from ejs to adminZWidgets

--- public/zonesWidgets/controllers/adminZWidgets.js
+++ public/zonesWidgets/controllers/adminZWidgets.js
@@ -7,3 +7,10 @@ var socket = io.connect(document.domain+':8080');
 updateVisibleZones = function(idWidget, active) {
     socket.emit('updateVisibility', {context: {idWidget: idWidget}, data: {active: active}});
 };
+
+function update(idWidget, nom) {
+
+		var active = document.getElementById(nom.id).checked;
+		updateVisibleZones(idWidget, active);
+
+}
