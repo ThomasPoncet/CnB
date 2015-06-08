dfc0d16b654aeb20d33f96f112c256ad9061fd72
Lucas Bchini <lucas.bchini@ensimag.grenoble-inp.fr>
2015-06-08 19:17:38 +0200

    videos modified, bug : don't go to the next one

--- widgets/videos/controllers/visitor.js
+++ widgets/videos/controllers/visitor.js
@@ -5,9 +5,10 @@ var widgetVisitor = require('../../../controllers/widgetVisitor.js');
 
 exports.run = function (req, res, connection) {
     // TODO idWidget
-    widgetVisitor.run(connection, {idWidget: 3}, function(data){
-        res.render("videosVisitor", {context: {sessionId: req.visitorSession.idSession, idWidget: 3},
-                                    data: data});
+    widgetVisitor.run(connection, {idWidget: 3}, function(info){
+        res.render("videosVisitor", {context: {sessionId: req.visitorSession.idSession, idWidget: 3,
+                                                activeWidgetsList: info.context.activeWidgetsList},
+                                    data: info.data});
     });
 };
 
